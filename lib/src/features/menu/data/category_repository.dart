8bdd381365172a96edc4b096886757f0bd6aafd6
Ralina <seitovaralina1@gmail.com>
2025-05-17 18:37:52 +0600

    feature: add menu category and item models with sample data

---
+++ lib/src/features/menu/data/category_repository.dart
@@ -0,0 +1,9 @@
+import '../models/menu_category.dart';
+
+List<MenuCategory> categories = [
+  MenuCategory(id: 0, name: 'Черный кофе'),
+  MenuCategory(id: 1, name: 'Кофе с молоком'),
+  MenuCategory(id: 2, name: 'Чай'),
+  MenuCategory(id: 3, name: 'Авторские напитки'),
+  MenuCategory(id: 4, name: 'Десерты'),
+];
---
+++ lib/src/features/menu/models/menu_category.dart
@@ -0,0 +1,6 @@
+class MenuCategory {
+  final int id;
+  final String name;
+
+  MenuCategory({required this.id, required this.name});
+}
