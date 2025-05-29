202bb3212e9a270024f0247d7159718e0ac4b157
Ralina <seitovaralina1@gmail.com>
2025-05-29 18:42:35 +0600

    feature: implement order data source and repository

---
+++ lib/src/features/order/data/order_repository.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+
+import '../models/dto/order_request_dto.dart';
+import 'data_sources/order_data_source.dart';
+
+abstract interface class IOrderRepository {
+  Future<void> submitOrder(Map<int, int> positions, String fcmToken);
+}
+
+final class OrderRepository implements IOrderRepository {
+  final IOrderDataSource _networkOrderDataSource;
+
+  const OrderRepository({
+    required IOrderDataSource networkOrderDataSource,
+  }) : _networkOrderDataSource = networkOrderDataSource;
+
+  @override
+  Future<void> submitOrder(Map<int, int> positions, String fcmToken) async {
+    final stringMap = positions.map((key, value) => MapEntry(key.toString(), value));
+    final dto = OrderRequestDto(
+      positions: stringMap,
+      token: fcmToken,
+    );
+    try {
+      await _networkOrderDataSource.createOrder(dto);
+    } on DioException catch (e) {
+      throw Exception('Failed to submit order: $e');
+    }
+  }
+}
