7b7f0f996884c0313c6fd46fdaff262853725ea9
Ralina <seitovaralina1@gmail.com>
2025-06-02 10:59:48 +0600

    feature: add menuDb database

---
+++ lib/src/common/database/database.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
+
+part 'database.g.dart';
+
+class MenuCategories extends Table {
+  IntColumn get id => integer()();
+  TextColumn get name => text()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class MenuItems extends Table {
+  IntColumn get id => integer()();
+  TextColumn get name => text()();
+  TextColumn get description => text().nullable()();
+  TextColumn get imageUrl => text().nullable()();
+  IntColumn get categoryId => integer().references(MenuCategories, #id)();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class MenuItemPrices extends Table {
+  IntColumn get itemId => integer().references(MenuItems, #id)();
+  TextColumn get currency => text()();
+  RealColumn get value => real()();
+
+  @override
+  Set<Column> get primaryKey => {itemId, currency};
+}
+
+@DriftDatabase(tables: [MenuCategories, MenuItems, MenuItemPrices])
+class MenuDb extends _$MenuDb {
+  MenuDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+
+    final cachebase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cachebase;
+
+    return NativeDatabase.createInBackground(file);
+  });
+}
