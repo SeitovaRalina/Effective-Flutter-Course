1ddebed5f0a9315b8a01b3fb1fbd03e931011e02
Ralina <seitovaralina1@gmail.com>
2025-06-02 11:01:45 +0600

    feature: integrate savable data sources for categories and menu items

--- lib/src/features/menu/data/category_repository.dart
+++ lib/src/features/menu/data/category_repository.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 
 import '../models/dto/menu_category_dto.dart';
 import '../models/menu_category.dart';
 import '../utils/category_mapper.dart';
 import 'data_sources/categories_data_source.dart';
+import 'data_sources/savable_categories_data_source.dart';
 
 abstract interface class ICategoryRepository {
   Future<List<MenuCategory>> loadCategories();
@@ -11,18 +14,26 @@ abstract interface class ICategoryRepository {
 
 final class CategoriesRepository implements ICategoryRepository {
   final ICategoriesDataSource _networkCategoriesDataSource;
+  final ISavableCategoriesDataSource _dbCategoriesDataSource;
 
   const CategoriesRepository({
     required ICategoriesDataSource networkCategoriesDataSource,
-  }) : _networkCategoriesDataSource = networkCategoriesDataSource;
+    required ISavableCategoriesDataSource dbCategoriesDataSource,
+  }) : _networkCategoriesDataSource = networkCategoriesDataSource,
+       _dbCategoriesDataSource = dbCategoriesDataSource;
 
   @override
   Future<List<MenuCategory>> loadCategories() async {
     var dtos = <MenuCategoryDto>[];
     try {
       dtos = await _networkCategoriesDataSource.fetchCategories();
+      _dbCategoriesDataSource.saveCategories(categories: dtos);
     } on DioException catch (e) {
-      throw Exception('Failed to load categories: $e');
+      if (e.error is SocketException) {
+        dtos = await _dbCategoriesDataSource.fetchCategories();
+      } else {
+        throw Exception('Failed to load categories: $e');
+      }
     }
     return dtos.map((e) => e.toModel()).toList();
   }
